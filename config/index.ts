76fea5d839de93f12a335c230b862a70277e55f4
RoadHero <132873551+RoadheroGB@users.noreply.github.com>
2023-07-11 13:59:35 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
+export const APP_ID = '8932fb99-f4a2-48eb-b33f-a070ffe8acdb'
+export const API_KEY = 'app-4E4fIAGrUYZgo8SlZ1mFyS4t'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
